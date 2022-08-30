2713c3821f1e1d406d3a050580077f41540b70ed
Ajay Pradhan <ajay.pradhan@metacube.com>
2022-08-30 21:52:53 +0530

    feature: anonymous method example

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace example;
+public delegate void anonymous();
+public class Program {
+    public static void Main(string[] args) {
+        anonymous f1 = delegate() {
+            Console.WriteLine("This is an anonymous method!");
+        };
+        f1();
+    }
+}
